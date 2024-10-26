6f0f2c3aed73407f005e143c26b4a181527678fe
luizfernandodag <luizfernandodag@gmail.com>
2024-10-26 16:34:32 -0300

    ini project spring

---
+++ click-bus/src/main/java/com/luiz/click_bus/ClickBusApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.click_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickBusApplication.class, args);
+	}
+
+}
---
+++ click-bus/src/test/java/com/luiz/click_bus/ClickBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.click_bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
